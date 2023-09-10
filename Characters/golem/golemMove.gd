a86210c61cb43b90ef5caf47003a4a5ea4d037ec
Darr3n2GG <139633310+Darr3n2GG@users.noreply.github.com>
2023-09-10 13:39:10 +0800

    build 0.22.1
    -reorganized code of golem and player

--- Characters/golem/golemMove.gd
+++ Characters/golem/golemMove.gd
@@ -2,62 +2,74 @@ extends CharacterBody2D
 
 @onready var player = get_node("../Player")  #get player node
 @onready var timer = $Timer #get timer node
-enum GolemStates {FOLLOW_STATE,SOLID_STATE,CARRY_STATE}
-@export var state = GolemStates.FOLLOW_STATE
+enum {
+	FOLLOW,
+	SOLID,
+	CARRY,
+	THROW
+	}
+@export var state = FOLLOW
 const JUMP_VEL : float = -300.0
 const SPEED : float = 200.0
-var carried : bool #check if golem is JUST carried
-var throwed : bool #check if golem is throwed
-var dir : int
+var throw_dir : int
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var dist : float
 
 func _physics_process(delta):
-	if not is_on_floor() and state != GolemStates.CARRY_STATE:
-		velocity.y += gravity * delta
-	var selfpos = global_position
 	var playerpos = player.global_position
+	var selfpos = global_position
 	var distx = abs(selfpos.x - playerpos.x)
 	var disty = abs(selfpos.y - playerpos.y)
 	var dyn_dir = (playerpos.x-selfpos.x)/abs(playerpos.x-selfpos.x)
-	if dir == 0:
-		dir = -1 if player.get_child(0).is_flipped_h() else 1
+		
+	if not is_on_floor() and state != CARRY:
+		velocity.y += gravity * delta
+		
 	if disty > 500:
 		global_position = player.global_position
-	elif distx > 50 and state == GolemStates.FOLLOW_STATE:
-		global_position.x += SPEED * dyn_dir * delta
-	elif state == GolemStates.CARRY_STATE:
-		global_position = Vector2(playerpos.x,playerpos.y - 30)
-	if not is_on_floor() and throwed == true:
-		global_position.x += SPEED * 1.25 * dir * delta
-	else:	
-		throwed = false
-		dir = 0
+		
+	match state:
+		FOLLOW:
+			if distx > 50:
+				global_position.x += SPEED * dyn_dir * delta
+		CARRY:
+			global_position = Vector2(playerpos.x,playerpos.y - 30)
+			
+		THROW:
+			if throw_dir == 0:
+				throw_dir = -1 if player.get_child(0).is_flipped_h() else 1
+			global_position.x += SPEED * 1.25 * throw_dir * delta
+			if is_on_floor():
+				throw_dir = 0
+				change_state(SOLID)
+		SOLID:
+			pass
 	move_and_slide()
 
 func _input(_event):
-	var dist = position.distance_to(player.position)
+	dist = position.distance_to(player.position)
 	if dist < 200:
-		if Input.is_action_just_pressed("interact golem") and state != GolemStates.CARRY_STATE:
+		if Input.is_action_just_pressed("interact golem") and state != CARRY:
 			timer.start()
 		elif Input.is_action_just_released("interact golem"):
-			if carried == false:
-				change_state(GolemStates.SOLID_STATE if state == GolemStates.FOLLOW_STATE else GolemStates.FOLLOW_STATE)
-			elif carried == true and timer.time_left == 0:
-				change_state(GolemStates.FOLLOW_STATE)
+			if state != CARRY and state != THROW:
+				change_state(SOLID if state == FOLLOW else FOLLOW)
+			elif state == CARRY and timer.time_left == 0:
+				change_state(FOLLOW)
 			timer.stop()
-		elif Input.is_action_just_pressed("jump") and state == GolemStates.CARRY_STATE:
-			change_state(GolemStates.SOLID_STATE)
-			velocity.y = JUMP_VEL
-			throwed = true
+		elif Input.is_action_just_pressed("jump") and state == CARRY:
+			change_state(THROW)
 
 func change_state(stated):
 	state = stated
-	set_collision_layer_value(2 if stated != GolemStates.FOLLOW_STATE else 1,0)
-	set_collision_layer_value(1 if stated != GolemStates.FOLLOW_STATE else 2,1)
-	set_collision_mask_value(2 if stated != GolemStates.FOLLOW_STATE else 1,0)
-	set_collision_mask_value(1 if stated != GolemStates.FOLLOW_STATE else 2,1)
-	carried = false if stated != GolemStates.CARRY_STATE else true
+	set_collision_layer_value(2 if stated != FOLLOW else 1,0)
+	set_collision_layer_value(1 if stated != FOLLOW else 2,1)
+	set_collision_mask_value(2 if stated != FOLLOW else 1,0)
+	set_collision_mask_value(1 if stated != FOLLOW else 2,1)
+	if stated == THROW:
+		velocity.y = JUMP_VEL
 
 func _on_timer_timeout():
-	change_state(GolemStates.CARRY_STATE)
+	if dist < 50:
+		change_state(CARRY)
 	
